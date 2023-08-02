ff109ad304adfca0e4572472fc3e8f2792b2eda0
Getachew Muhabaw <getachewmuhabaw@gmail.com>
2023-08-02 10:14:53 +0300

    Create django apps

--- Preventive_Maintainace/asgi.py
+++ Preventive_Maintainace/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for pm project.
+ASGI config for Preventive_Maintainace project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pm.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Preventive_Maintainace.settings')
 
 application = get_asgi_application()
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pm.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Preventive_Maintainace.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
