8a8d17ae4a768c66efb6a97e39d96b6e13f34cc8
Getachew Muhabaw <getachewmuhabaw@gmail.com>
2023-09-18 14:33:03 +0300

    Create models

--- pm/models.py
+++ pm/models.py
@@ -4,3 +4,28 @@ from django.db import models
 class Bank(models.Model):
     bank_name =models.CharField(max_length=50)
     bank_key = models.CharField(max_length=40)
+
+    def __str__(self):
+            return self.bank_name
+        
+
+class Region(models.Model):
+   REGION_CHIOCES = [
+    ("NR", "North"),
+    ("SR", "South"),
+    ("ER", "East"),
+    ("WR", "West"),
+    ("CR", "Centeral"),
+    ("NN","None")
+]
+   name = models.CharField(max_length=30)
+   region = models.CharField(max_length=2,choices=REGION_CHIOCES)
+   def __str__(self):
+       return self.name
+   
+class BankBranch(models.Model):
+    bank_name = models.ForeignKey(Bank, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.bank_name
+    
