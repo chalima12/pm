297f35f555daeec4107e1321466c949d691aa582
Getachew Muhabaw <getachewmuhabaw@gmail.com>
2023-10-20 12:43:41 +0300

    Registring the Model for Admin

--- pm/admin.py
+++ pm/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
-from .models import *
+from pm.models import Bank,Schedule,Terminal,User
 
-admin.site.register(Bank)
-admin.site.register(User)
-admin.site.register(Terminal)
-admin.site.register(Schedule)
+# admin.site.register(Bank)
+# admin.site.register(User)
+# admin.site.register(Terminal)
+# admin.site.register(Schedule)
+@admin.register(Bank)
+class BankAdmin(admin.ModelAdmin):
+    pass
 
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Schedule)
+class ScheduleAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Terminal)
+class TerminalAdmin(admin.ModelAdmin):
+    pass
 
