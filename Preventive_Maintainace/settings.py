7b965cbeda2a6e61970a4f617ae73a7fbfdfc24c
Getachew Muhabaw <getachewmuhabaw@gmail.com>
2023-10-28 08:06:21 +0300

    Working on Login Fronted

--- Preventive_Maintainace/settings.py
+++ Preventive_Maintainace/settings.py
@@ -39,11 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pm',
-    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,11 +53,6 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'Preventive_Maintainace.urls'
-INTERNAL_IPS = [
-    # ...
-    "127.0.0.1",
-    # ...
-]
 
 TEMPLATES = [
     {
