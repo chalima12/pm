5d9daa96dfee626fe18eb58b50a36ca5dfd4f40f
Getachew Muhabaw <getachewmuhabaw@gmail.com>
2023-11-21 13:39:49 +0300

    Login and logout page

--- testing.py
+++ testing.py
@@ -54,3 +54,69 @@ hour = diff.hours
                         #   style="width: 90px"
                         #   class="d-inline p-2 m-3 btn btn-block btn-danger"
                         #   ><i class="fa">End</i></a>
+
+
+
+# {% load static %}
+# <!--Login Form-->
+# <!DOCTYPE html>
+# <html lang="en">
+# <head>
+#   <meta charset="utf-8">
+#   <meta name="viewport" content="width=device-width, initial-scale=1">
+#   <title>Moti Engineering</title>
+
+#   <!-- Google Font: Source Sans Pro -->
+#   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+#   <!-- Font Awesome -->
+#   <link rel="stylesheet" href="{% static "plugins/fontawesome-free/css/all.min.css" %}">
+#   <!-- icheck bootstrap -->
+#   <link rel="stylesheet" href="{% static "plugins/icheck-bootstrap/icheck-bootstrap.min.css" %}">
+#   <!-- Theme style -->
+#   <link rel="stylesheet" href="{% static "dist/css/adminlte.min.css" %}">
+# </head>
+# <body class="hold-transition login-page">
+# <div class="login-box" >
+#   <h1>Moti Engineering</h1>
+#       </div>
+#   <div class="card card-outline card-success">
+#     <div class="card-header text-center">
+#       <h2>PM Portal</h2>
+#     </div>
+#     <div class="card-body">
+#       <p class="login-box-msg">Sign in to start your session</p>
+
+#       <form action="" method="post">
+#         {% csrf_token %}
+#         <div class="row px-5 ">
+#           <div class = "col-10">
+#               {{form.as_p}}
+#           </div>
+#           <!-- /.col -->
+#          {{form.password}}
+#          {{form.username}}
+#           <!-- /.col -->
+#         </div>
+#          <div class="col-12">
+#             <button type="submit" class="btn btn-primary btn-block" style="background-color: rgb(114, 231, 141);color: black;">Sign In</button>
+#           </div>
+#       </form>
+#     </div>
+
+#     <!-- /.card-body -->
+#   </div>
+#   <!-- /.card -->
+#       <div class ="card-footer text-black display-5 ">
+# <p>THE LEADING IT & BANKING AUTOMATION SOLUTION PROVIDER IN ETHIOPIA</p>
+#     </div>
+# </div>
+# <!-- /.login-box -->
+
+# <!-- jQuery -->
+# <script src="{% static "plugins/jquery/jquery.min.js" %}"></script>
+# <!-- Bootstrap 4 -->
+# <script src="{% static "plugins/bootstrap/js/bootstrap.bundle.min.js" %}"></script>
+# <!-- AdminLTE App -->
+# <script src="{% static "dist/js/adminlte.min.js" %}"></script>
+# </body>
+# </html>
