cf495d5b9f3c528518b2a2e01e86071242d0bc10
Getachew Muhabaw <getachewmuhabaw@gmail.com>
2023-12-08 09:44:35 +0300

    Context variable passed to base html

--- pm/context_processors.py
+++ pm/context_processors.py
@@ -4,18 +4,22 @@ import json
 
 def global_context(request):
     #TODO: Context Passed form Schedule LIst and 
-    onprogressSchedule = Schedule.objects.filter(status="OP").count()
-    commpletedSchedule = Schedule.objects.filter(status ="CO").count()
     pendingSchedule = Schedule.objects.filter(status="PE").count()
     waitingSchedule = Schedule.objects.filter(status="WT").count()
+    onprogressSchedule = Schedule.objects.filter(status="OP").count()
+    submittedSchedule = Schedule.objects.filter(status ="CO").count()
+    approvedSchedule = Schedule.objects.filter(status="AP").count()
+    rejectedSchedule = Schedule.objects.filter(status="RE").count()
     
-    allSchedule = onprogressSchedule+commpletedSchedule+pendingSchedule+waitingSchedule
+    allSchedule = pendingSchedule + waitingSchedule + onprogressSchedule + submittedSchedule + approvedSchedule + rejectedSchedule
     context = {
         "allSchedule":allSchedule,
         "pendingSchedule": json.dumps(pendingSchedule),
         "waitingSchedule": json.dumps(waitingSchedule),
         "onprogressSchedule": json.dumps(onprogressSchedule),
-        "commpletedSchedule": json.dumps(commpletedSchedule),
+        "submittedSchedule": json.dumps(submittedSchedule),
+        "approvedSchedule": json.dumps(approvedSchedule),
+        "rejectedSchedule": json.dumps(rejectedSchedule),
     }
     return context 
     
