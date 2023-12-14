d8eab8341c605cf4eab42d2fe272ab024e15a8d0
Getachew Muhabaw <getachewmuhabaw@gmail.com>
2023-12-14 11:31:37 +0300

    Working Permissions

--- Preventive_Maintainace/urls.py
+++ Preventive_Maintainace/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('pm.urls')),
-    path("__debug__/", include("debug_toolbar.urls")),
+    # path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
