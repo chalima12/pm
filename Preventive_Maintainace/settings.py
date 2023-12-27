21d42968761863db958f2537c12572ee515d5579
Getachew Muhabaw <getachewmuhabaw@gmail.com>
2023-12-27 10:09:51 +0300

    Working on connection on postgress

--- Preventive_Maintainace/settings.py
+++ Preventive_Maintainace/settings.py
@@ -78,10 +78,22 @@ WSGI_APPLICATION = 'Preventive_Maintainace.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+#Postgress setup 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pm',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
